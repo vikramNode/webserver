9a68f29e002f11566d7880405ee84bac98f0b8db
vikram <vikram.sethumadhavan@indegene.com>
2016-01-03 22:27:04 +0530

    final version

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', middleware.requireAuthentication, function(req,res){
     res.send('Welcome to Express');
 });
 
+app.get('/about', middleware.requireAuthentication, function(req,res){
+    res.send('Welcome to Express');
+});
+
 app.listen(PORT, function(){
     console.log("Express Server Started on port " + PORT + "!");
 });
