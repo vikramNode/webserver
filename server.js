bab95be0513a69f6a94052e56f5c4c7172561e9a
vikram <vikram.sethumadhavan@indegene.com>
2016-01-03 22:26:08 +0530

    final version

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-var Express = require('express');
+var express = require('express');
+var app = express();
+var PORT = process.env.port || 3000;
+
+var middleware = require('./middleware');
+
+app.get('/', middleware.requireAuthentication, function(req,res){
+    res.send('Welcome to Express');
+});
+
+app.listen(PORT, function(){
+    console.log("Express Server Started on port " + PORT + "!");
+});
