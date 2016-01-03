23bec40b472c4c02ce5c43f6750e83331ad30371
vikram <vikram.sethumadhavan@indegene.com>
2016-01-03 23:10:14 +0530

    Heroku Version

--- app.js
+++ app.js
@@ -2,17 +2,13 @@ var express = require('express');
 var app = express();
 var PORT = process.env.port || 3000;
 
-var middleware = require('./middleware');
 
-app.get('/', middleware.requireAuthentication, function(req,res){
+
+app.get('/about', function(req,res){
     res.send('Welcome to Express');
 });
 
-app.get('/about', middleware.requireAuthentication, function(req,res){
-    res.send('Welcome to Express');
-});
-
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function(){
     console.log("Express Server Started on port " + PORT + "!");
